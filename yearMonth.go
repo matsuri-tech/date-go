77019f7cc5d89df5615caa4d831d2e3b20d79741
irieryota <ryotairie123@gmail.com>
2020-07-31 18:21:15 +0900

    feat(month): add remaining month

--- yearMonth.go
+++ yearMonth.go
@@ -12,9 +12,18 @@ type YearMonth struct {
 type YearMonths []YearMonth
 
 const (
-	January  Month = 1
-	August   Month = 8
-	December Month = 12
+	January   Month = 1
+	February  Month = 2
+	March     Month = 3
+	April     Month = 4
+	May       Month = 5
+	June      Month = 6
+	July      Month = 7
+	August    Month = 8
+	September Month = 9
+	October   Month = 10
+	November  Month = 11
+	December  Month = 12
 )
 
 const (
